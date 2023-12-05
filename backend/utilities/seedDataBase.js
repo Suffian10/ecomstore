6415d262ec7479e88a7552b6982c7a6cccceeebf
Muhammad Suffian <msuffiansoomro@gmail.com>
2023-12-05 22:31:57 +0500

    few changes

--- backend/utilities/seedDataBase.js
+++ backend/utilities/seedDataBase.js
@@ -14,11 +14,11 @@ const connection = mysql.createConnection({
 
 // Sample user data
 const users = [
-    { id: 1, username: 'john_doe', email: 'john@example.com', password: 'password123' },
-    { id: 2, username: 'jane_smith', email: 'jane@example.com', password: 'pass456' },
-    { id: 3, username: 'alice_wonder', email: 'alice@example.com', password: 'alicepass789' },
-    { id: 4, username: 'bob_marley', email: 'bob@example.com', password: 'bobpass' },
-    { id: 5, username: 'emma_jones', email: 'emma@example.com', password: 'emmapassword' }
+    {username: 'john_doe', email: 'john@gmail.com', password: 'password123' },
+    {username: 'jane_smith', email: 'jane@gmail.com', password: 'pass456' },
+    {username: 'alice_wonder', email: 'alice@gmail.com', password: 'alicepass789' },
+    {username: 'bob_marley', email: 'bob@gmail.com', password: 'bobpass' },
+    {username: 'emma_jones', email: 'emma@gmail.com', password: 'emmapassword' }
 ];
 
 // Connect to the database
@@ -30,8 +30,8 @@ connection.connect((err) => {
     console.log('Connected to the database');
 
     // Insert user data into the users table
-    const sql = 'INSERT INTO users (id, username, email, password) VALUES ?';
-    const values = users.map(user => [user.id, user.username, user.email, user.password]);
+    const sql = 'INSERT INTO users (user_name, user_email, user_password) VALUES ?';
+    const values = users.map(user => [user.username, user.email, user.password]);
 
     connection.query(sql, [values], (error, results) => {
         if (error) {
